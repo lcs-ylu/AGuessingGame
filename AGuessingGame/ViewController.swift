5e99fe1d7a94215ec9d6bf5a094d37b80faeee9c
Nancy Lu <ylu@lcs.on.ca>
2019-09-30 11:09:55 -0400

    Initial Commit

---
+++ AGuessingGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AGuessingGame
+//
+//  Created by Lu, Yunjia on 2019-09-30.
+//  Copyright © 2019 Nancy Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
